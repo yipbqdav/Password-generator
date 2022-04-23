ca33b5113f01e7dd6c9eada2ff19ce9efdf4b087
yipbqdav <yipbqdav@gmail.com>
2022-04-23 15:42:09 -0700

    seeing if this works
    added js and variables

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,12 +3,42 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = writePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+  var charSets = {
+    lowercase: 'abcdefghijklmnopqrstuvwxyz' ,
+    uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' ,
+    numeric: '1234567890' ,
+    special: ' !"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~' ,
+  };
+  return null;
+}
 
+function passGen(length){
+  var pass = '';
+  var chars=''
+  if (upper = true){
+    chars += uppercase;
+  }
+  if (lower = true){
+    chars += lowercase;
+  }
+  if (numeric = true){
+    chars += numeric
+  }
+  if (special = true){
+    chars += special;
+  }
+  for (let i = 0; i < length; i++){
+    pass[i] = chars.charAt(Math.random()* Math.random())
+  }
+  return pass;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+var length = Number(prompt("Enter a password length between 8 and 20")),
+  charType = prompt("Enter a character type: special, numeric, uppercase, lowercase."),
+  password = writePassword();
